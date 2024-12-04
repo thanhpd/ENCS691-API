4b9928e2ba324a96a7372944f133a9e9c4a500e1
Thanh Phan <ph_thanh@live.concordia.ca>
2024-12-03 22:21:54 -0500

    fix: remove unused modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,8 +10,6 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { ConfigService } from '@nestjs/config';
 import { AuctionModule } from './auction/auction.module';
-import { BiddingItemModule } from './bidding-item/bidding-item.module';
-import { LotModule } from './lot/lot.module';
 import { AuctionLotModule } from './auction-lot/auction-lot.module';
 import { BidModule } from './bid/bid.module';
 
@@ -28,8 +26,6 @@ import { BidModule } from './bid/bid.module';
     AuthModule,
     UserModule,
     AuctionModule,
-    BiddingItemModule,
-    LotModule,
     AuctionLotModule,
     BidModule,
   ],
