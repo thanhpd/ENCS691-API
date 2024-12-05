6353a2baf74d66a6c43df802a714cb8cc7af7c09
Thanh Phan <ph_thanh@live.concordia.ca>
2024-12-05 10:42:48 -0500

    fix: update healthcheck public endpoint

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -4,6 +4,7 @@ import {
   HttpHealthIndicator,
   HealthCheck,
 } from '@nestjs/terminus';
+import { Public } from 'src/common/decorators/public.decorator';
 
 @Controller('health')
 export class HealthController {
@@ -12,6 +13,7 @@ export class HealthController {
     private http: HttpHealthIndicator,
   ) {}
 
+  @Public()
   @Get()
   @HealthCheck()
   check() {
